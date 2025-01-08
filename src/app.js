a842bb0b68d603dd8902c3fbb38a5b0971e724b7
RajWalke <rajwalke66@gmail.com>
2025-01-08 22:39:48 +0530

    API call checking is done

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express=require("express");
 
 const app=express();
-app.use("/home",(req,res)=>{
-    res.send("It is my home page");
+app.use("/tests",(req,res)=>{
+    res.send("Im in use case why???")
 })
-app.use("/about",(req,res)=>{
-    res.send("Its is my about page")
+app.get("/test",(req,res)=>{
+    res.send({firstName:"Raj Walke",age:21})
 })
-app.use((req,res)=>{
+
+app.post("/test",(req,res)=>{
+    res.send("It is Postr methode for testing")
+})
+
+app.delete("/test",(req,res)=>{
+    res.send("It is Delete methode for testing");
+})
+// app.use("/home/route",(req,res)=>{
+//     res.send("It is my home routing page");
+// })
+// app.use("/home",(req,res)=>{
+//     res.send("It is my home page");
+// })
+
+// app.use("/about",(req,res)=>{
+//     res.send("Its is my about page")
+// })
+app.get("/",(req,res)=>{
     res.send("Hello Namste Everyone");
 })
 
@@ -15,4 +33,4 @@ app.use((req,res)=>{
 
 app.listen(7777,()=>{
     console.log("NodeJS Server is Running");
-})
+});
