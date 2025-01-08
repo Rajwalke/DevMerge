c9fdbbb11f029b9b5d0d911fc07fba179fd9949e
RajWalke <rajwalke66@gmail.com>
2025-01-09 00:50:12 +0530

    rounting advance done

--- src/app.js
+++ src/app.js
@@ -1,20 +1,42 @@
 const express=require("express");
 
 const app=express();
-app.use("/tests",(req,res)=>{
-    res.send("Im in use case why???")
-})
-app.get("/test",(req,res)=>{
-    res.send({firstName:"Raj Walke",age:21})
-})
+// app.use("/test",(req,res)=>{
+//     res.send("Im in use case why???")
+// })
+// LETS CHECK THE ROUNTING IN ADAVANCE 
 
-app.post("/test",(req,res)=>{
-    res.send("It is Postr methode for testing")
-})
+// app.get("/abd(Raj)?c",(req,res)=>{
+//     res.send({firstName:"Raj Walke",age:21})
+// })
 
-app.delete("/test",(req,res)=>{
-    res.send("It is Delete methode for testing");
-})
+// app.get("/a(d)b+c",(req,res)=>{
+//     res.send({firstname:"Raj",lastName:"Walke"});
+// });
+
+// app.get("/ij*k",(req,res)=>{
+//     res.send({firstname:"mansi",lastName:"Dhale"});
+// })
+
+// app.get(/a/,(req,res)=>{
+//     res.send({firstname:"Raj",lastName:"Walke"});
+// });
+// app.get(/.*fly$/,(req,res)=>{
+//     res.send({firstname:"Raj",lastName:"Walke"});
+// })
+
+app.get("/user/:userId/:userName/:pass",(req,res)=>{
+    res.send(req.params);
+}
+);
+
+// app.post("/test",(req,res)=>{
+//     res.send("It is Postr methode for testing")
+// })
+
+// app.delete("/test",(req,res)=>{
+//     res.send("It is Delete methode for testing");
+// })
 // app.use("/home/route",(req,res)=>{
 //     res.send("It is my home routing page");
 // })
