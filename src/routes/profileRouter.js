15f1f61723139905191b3a2bdfa142bc19c0beeb
RajWalke <rajwalke66@gmail.com>
2025-02-01 22:43:37 +0530

    editPassword APIs is complete

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -4,7 +4,7 @@ const profileRouter=express.Router();
 const {userAuth}=require("../middlewere/authorized");
 const { UserInfo } = require('../models/user');
 const {validationProfileEditData}=require("../utils/validation");
-
+const bcrypt=require('bcrypt');
 
 // Profile of user
 profileRouter.get("/profile/view",userAuth,async(req,res)=>{
@@ -42,5 +42,27 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
     }catch(err){
         res.status(404).send("Error: "+ err)
     }
+})
+// Password edit
+profileRouter.patch("/profile/editPassword",userAuth,async(req,res)=>{
+    const user=req.user;
+    const {password,newpassword}=req.body;
+    const hashPassword=user.password;
+    try{
+        const isPasswordMatch=await bcrypt.compare(password,hashPassword);
+        if(!isPasswordMatch){
+            throw new Error("Incorrect Password");
+        }
+        // res.send("password is match");
+        const newHashPassword=await bcrypt.hash(newpassword,10);
+        user.password=newHashPassword;
+        user.save();
+        res.cookie("Token",null,{expires:new Date(Date.now())});
+        res.send("Password is change, Please LogIn with new Password");
+
+    }catch(err){
+        res.status(404).send("Error: "+err);
+    }
+
 })
 module.exports=profileRouter;
